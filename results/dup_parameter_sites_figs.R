c2b3aae2a67279b4e3e06b1ab6cf106286870b66
mmjohn <mmjohn@utexas.edu>
2021-08-23 18:19:40 -0500

    Cosmetic updates to figures

--- results/dup_parameter_sites_figs.R
+++ results/dup_parameter_sites_figs.R
@@ -15,6 +15,7 @@ library(tidyr)
 library(ggplot2)
 library(cowplot)
 library(ggtext)
+library(colorspace)
 
 
 #--------------- GLOBAL PARAMETERS --------------------
@@ -43,84 +44,48 @@ load(file.path(path_to_results, 'dup_analysis_sites_fixed_n_unsort_results.RData
 
 #--------------- FIXED MU SITES DISTRIBUTION FIGURES --------------------
 
-# figure showing seg. sites of duplicates compared to whole data set
+# combine data sets for faceted figure
 sites_mu_df %>% 
-  filter(mut_rate == 1.5e-8) %>% 
-  ggplot(aes(x = segsites, fill = status, color = status)) +
-  geom_bar(position = "stack") +
-  #geom_density_ridges(alpha = 0.5) +
-  facet_wrap(vars(pop_size), scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count"
-  ) +
-  scale_color_manual(values = c("#1b9e77", "#7570b3")) +
-  scale_fill_manual(values = c("#1b9e77", "#7570b3")) +
-  theme_bw() +
-  #theme_ridges() +
-  theme(
-    legend.title = element_blank()
-  ) -> fig_sites_dup_v_unq_fixed_mu
-
-fig_sites_dup_v_unq_fixed_mu
-
+  mutate(processing = "Sorted") -> mu_df1
 sites_mu_unsort_df %>% 
-  filter(mut_rate == 1.5e-8) %>% 
-  ggplot(aes(x = segsites, fill = status, color = status)) +
-  geom_bar(position = "stack") +
-  facet_wrap(vars(pop_size), scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count"
-  ) +
-  scale_color_manual(values = c("#1b9e77", "#7570b3")) +
-  scale_fill_manual(values = c("#1b9e77", "#7570b3")) +
-  theme_bw() +
-  theme(
-    legend.title = element_blank()
-  ) -> fig_sites_dup_v_unq_fixed_mu_unsort
-
-fig_sites_dup_v_unq_fixed_mu_unsort
-
-
-sites_mu_df %>% 
-  mutate(processing = "sorted") -> mu_df1
-sites_mu_unsort_df %>% 
-  mutate(processing = "unsorted") -> mu_df2
+  mutate(processing = "Unsorted") -> mu_df2
 full_join(mu_df1, mu_df2) -> sites_mu_full_df
 
-sites_mu_full_df %>% 
-  filter(mut_rate == 1.5e-8 & status == "duplicate") %>% 
-  ggplot(aes(x = segsites, fill = processing)) +
-  geom_bar(position = "dodge") +
-  #geom_density_ridges(alpha = 0.5) +
-  facet_wrap(vars(pop_size), scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count"
-  ) +
-  scale_fill_manual(values = c("#1b9e77", "#7570b3")) +
-  theme_bw() +
-  theme(
-    legend.title = element_blank()
-  ) -> fig_sites_sort_v_unsort_fixed_mu
+# manually set labels
+labels_n <- c(
+  "100" = "N = 100", 
+  "316" = "N = 316",
+  "1000" = "N = 1,000"
+)
 
+# figure showing seg. sites of duplicates compared to whole data set
 sites_mu_full_df %>% 
   filter(mut_rate == 1.5e-8) %>% 
   filter(pop_size == 100 | pop_size == 316 | pop_size == 1000) %>% 
   ggplot(aes(x = segsites, fill = status)) +
   geom_bar(position = "stack") +
-  #geom_density_ridges(alpha = 0.5) +
-  facet_wrap(processing ~ pop_size, scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count",
-    fill = "Processing"
+  facet_grid(
+    rows = vars(processing),
+    cols = vars(pop_size),
+    labeller = labeller(pop_size = labels_n)
   ) +
-  scale_fill_manual(values = c("#7570b3", "#1b9e77")) +
-  theme_bw() +
+  scale_fill_discrete_qualitative(
+    palette = "Cold",
+    name = "Identity",
+    labels = c("Duplicate", "Unique")
+  ) +
+  coord_cartesian(xlim = c(-0.2,10)) +
+  scale_x_continuous(
+    breaks = c(0, 2, 4, 6, 8, 10),
+    name = "Number of segregating sites"
+  ) +
+  scale_y_continuous(
+    expand = expansion(mult = c(0, .05)), 
+    name = "Count"
+  ) +
+  theme_bw(12) +
   theme(
-    legend.title = element_blank()
+    strip.text = element_markdown()
   ) -> fig_sites_sort_v_unsort_fixed_mu
 
 fig_sites_sort_v_unsort_fixed_mu
@@ -128,119 +93,53 @@ fig_sites_sort_v_unsort_fixed_mu
 
 #--------------- FIXED N SITES DISTRIBUTION FIGURES --------------------
 
+# combine data sets for faceted figure
 sites_n_df %>% 
-  filter(pop_size == 10000) %>% 
-  ggplot(aes(x = segsites, fill = status, color = status)) +
-  #geom_histogram(alpha = 0.5, position = "dodge") +
-  geom_bar(position = "stack") +
-  facet_wrap(vars(mut_rate), scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count"
-  ) +
-  scale_color_manual(values = c("#1b9e77", "#7570b3")) +
-  scale_fill_manual(values = c("#1b9e77", "#7570b3")) +
-  theme_bw() +
-  theme(
-    legend.title = element_blank()
-  ) -> fig_sites_dup_v_unq_fixed_n
-
-fig_sites_dup_v_unq_fixed_n
-
+  mutate(processing = "Sorted") -> n1_df
 sites_n_unsort_df %>% 
-  filter(pop_size == 10000) %>% 
-  ggplot(aes(x = segsites, fill = status, color = status)) +
-  #geom_histogram(alpha = 0.5, position = "dodge") +
-  geom_bar(position = "stack") +
-  facet_wrap(vars(mut_rate), scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count"
-  ) +
-  scale_color_manual(values = c("#1b9e77", "#7570b3")) +
-  scale_fill_manual(values = c("#1b9e77", "#7570b3")) +
-  theme_bw() +
-  theme(
-    legend.title = element_blank()
-  ) -> fig_sites_dup_v_unq_fixed_n_unsort
-
-fig_sites_dup_v_unq_fixed_n_unsort
-
-
-sites_n_df %>% 
-  mutate(processing = "sorted") -> n1_df
-sites_n_unsort_df %>% 
-  mutate(processing = "unsorted") -> n2_df
+  mutate(processing = "Unsorted") -> n2_df
 full_join(n1_df, n2_df) -> sites_n_full_df
 
-sites_n_full_df %>% 
-  filter(pop_size == 1000 & status == "duplicate") %>% 
-  ggplot(aes(x = segsites, fill = processing)) +
-  geom_bar(position = "dodge") +
-  #geom_density_ridges(alpha = 0.5) +
-  facet_wrap(vars(mut_rate), scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count"
-  ) +
-  scale_fill_manual(values = c("#1b9e77", "#7570b3")) +
-  theme_bw() +
-  theme(
-    legend.title = element_blank()
-  ) -> fig_sites_sort_v_unsort_fixed_n
-
-fig_sites_sort_v_unsort_fixed_n
-
+# manually set labels
+labels_mu <- c(
+  "1e-10" = "&mu; = 1.0e-10", 
+  "3.16e-10" = "&mu; = 3.16e-10",
+  "1e-09" = "&mu; = 1.0e-9"
+)
 
 sites_n_full_df %>% 
   filter(pop_size == 10000) %>% 
   filter(mut_rate == 1e-10 | mut_rate == 3.16e-10 | mut_rate == 1e-9) %>% 
   ggplot(aes(x = segsites, fill = status)) +
   geom_bar(position = "stack") +
-  #geom_density_ridges(alpha = 0.5) +
-  facet_wrap(processing ~ mut_rate, scales = "free") +
-  labs(
-    x = "Number of segregating sites",
-    y = "Count",
-    fill = "Processing"
+  facet_grid(
+    rows = vars(processing),
+    cols = vars(mut_rate),
+    labeller = labeller(mut_rate = labels_mu)
   ) +
-  scale_fill_manual(values = c("#7570b3", "#1b9e77")) +
-  theme_bw() +
+  scale_fill_discrete_qualitative(
+    palette = "Cold",
+    name = "Identity",
+    labels = c("Duplicate", "Unique")
+  ) +
+  coord_cartesian(xlim = c(-0.2,10)) +
+  scale_x_continuous(
+    breaks = c(0, 2, 4, 6, 8, 10),
+    name = "Number of segregating sites"
+  ) +
+  scale_y_continuous(
+    expand = expansion(mult = c(0, .05)), 
+    name = "Count"
+  ) +
+  theme_bw(12) +
   theme(
-    legend.title = element_blank()
+    strip.text = element_markdown()
   ) -> fig_sites_sort_v_unsort_fixed_n
 
 fig_sites_sort_v_unsort_fixed_n
 
-#--------------- SAVE FIGURES --------------------
 
-# save_plot(
-#   file.path(path_to_results, 'figures', 'fig_sites_dup_fixed_mu.png'),
-#   fig_sites_dup_v_unq_fixed_mu, 
-#   ncol = 1, nrow = 1, base_height = 4.71,
-#   base_asp = 1.618, base_width = NULL
-# )
-# 
-# save_plot(
-#   file.path(path_to_results, 'figures', 'fig_sites_dup_fixed_mu_unsort.png'),
-#   fig_sites_dup_v_unq_fixed_mu_unsort, 
-#   ncol = 1, nrow = 1, base_height = 4.71,
-#   base_asp = 1.618, base_width = NULL
-# )
-# 
-# save_plot(
-#   file.path(path_to_results, 'figures', 'fig_sites_dup_fixed_n.png'),
-#   fig_sites_dup_v_unq_fixed_n, 
-#   ncol = 1, nrow = 1, base_height = 4.71,
-#   base_asp = 1.618, base_width = NULL
-# )
-# 
-# save_plot(
-#   file.path(path_to_results, 'figures', 'fig_sites_dup_fixed_n_unsort.png'),
-#   fig_sites_dup_v_unq_fixed_n_unsort, 
-#   ncol = 1, nrow = 1, base_height = 4.71,
-#   base_asp = 1.618, base_width = NULL
-# )
+#--------------- SAVE FIGURES --------------------
 
 save_plot(
   file.path(path_to_results, 'figures', 'fig_sites_sort_v_unsort_fixed_mu.png'),
