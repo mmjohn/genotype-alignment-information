7462b1349f56f411fdabe4e8e356b80f1905cdbc
mmjohn <mmjohn@utexas.edu>
2021-09-02 14:11:35 -0500

    Updated to torch model using 1 module instead of 3 to define multi-input branched model; changed filters values to match data

--- scripts/cnn_06_torch_model_train.R
+++ scripts/cnn_06_torch_model_train.R
@@ -4,8 +4,8 @@
 # Using:
 #   2 branch model from Flagel et al. (2018)
 #   60,000 simulations per set
-#   Currently using tensorflow via Keras and reticulate
-# This script: defines, compiles, and trains model; saves a hdf5
+#   Currently using PyTorch via torch 0.5.0
+# This script: defines, compiles, trains, and saves model
 # Mackenzie M. Johnson
 # August 2021
 
@@ -18,13 +18,7 @@ library(purrr)
 library(dplyr)
 library(tidyr)
 library(glue)
-library(reticulate)
-# call the conda environment that has keras and tensorflow installed
-use_condaenv(
-  "r-reticulate", 
-  conda = "/stor/home/mmj2238/.local/share/r-miniconda/bin/conda"
-)
-library(keras) 
+library(torch)
 
 # record session info
 sessionInfo()
@@ -36,9 +30,10 @@ num_sims <- 60000   # for all
 # num_sims <- 60000   # for low unq
 # num_sims <- 60000   # for high unq
 
-# paths to data
+# paths for script
 path_to_data <- "/stor/work/Wilke/mmj2238/rho_cnn_data/parsed/dup_analysis/cnn_dup/"
 path_to_results <- "/stor/home/mmj2238/genotype-alignment-information/results/"
+path_to_models <- "/stor/work/Wilke/mmj2238/trained_models/dup_analysis"
 
 # size of alignments
 num_chrom <- 50
@@ -53,175 +48,183 @@ load(file.path(path_to_data, 'model_data_low_dup_all.RData'))
 # load(file.path(path_to_data, 'model_data_high_dup_unq.RData'))
 
 
+#--------------- DATA TO TENSORS --------------------
+
+# should specify dtype 
+# NOTE: some cost functions require specific types of data
+# error messages should clearly state if this is an issue
+
+# alignments
+low_align_all_train_tensor <- torch_tensor(
+  low_align_all_train,
+  requires_grad = TRUE # this is required for nn training; tracks computations to calc. deriv.
+)
+
+low_align_all_val_tensor <- torch_tensor(
+  low_align_all_val,
+  requires_grad = TRUE 
+)
+
+low_align_all_test_tensor <- torch_tensor(
+  low_align_all_test,
+  requires_grad = TRUE 
+)
+
+rm(low_align_all_train, low_align_all_val, low_align_all_test)
+
+# positions
+low_pos_all_train_tensor <- torch_tensor(
+  low_pos_all_train,
+  requires_grad = TRUE 
+)
+
+low_pos_all_val_tensor <- torch_tensor(
+  low_pos_all_val,
+  requires_grad = TRUE 
+)
+
+low_pos_all_test_tensor <- torch_tensor(
+  low_pos_all_test,
+  requires_grad = TRUE 
+)
+
+rm(low_pos_all_train, low_pos_all_val, low_pos_all_test)
+
+# rhos
+low_rho_all_train_tensor <- torch_tensor(
+  low_rho_all_train_centered,
+  requires_grad = TRUE
+)
+
+low_rho_all_val_tensor <- torch_tensor(
+  low_rho_all_val_centered,
+  requires_grad = TRUE
+)
+
+low_rho_all_test_tensor <- torch_tensor(
+  low_rho_all_test_centered,
+  requires_grad = TRUE
+)
+
+rm(low_rho_all_train, low_rho_all_val, low_rho_all_test, low_rho_all_train_centered, 
+   low_rho_all_val_centered, low_rho_all_test_centered)
+
+
 #--------------- DEFINE MODEL --------------------
 
-# set l2 regularization parameter
-l2_lambda <- 0.0001
+# create model with nn_module()
 
-# define the inputs
-genotype_input <- layer_input(
-  shape = c(106, 50),               # 106 for low, 17 for high
-  dtype = 'float32',
-  name = 'genotype_input'
+# Flagel model:
+# NOTE: in Flagel code, first set of avg pooling and dropout is missing, but it's reported in paper
+
+# need to add regularizers
+
+flagel_cnn <- nn_module(
+  "class_net",
+
+  initialize = function() {
+
+    #branch 1 (alignment CNN)
+    self$conv1 <- nn_conv1d(
+      in_channels = 106,                    
+      out_channels = 1250,
+      kernel_size = 2
+    )
+    self$dropout1 <- nn_dropout(0.25)
+    self$conv2 <- nn_conv1d(
+      in_channels = 1250,
+      out_channels = 256,                  
+      kernel_size = 2
+    )
+    self$dropout2 <- nn_dropout(0.25)
+    self$conv3 <- nn_conv1d(
+      in_channels = 256,
+      out_channels = 256,                   
+      kernel_size = 2
+    )
+    self$dropout3 <- nn_dropout(0.25)
+
+    # branch 2 (position fc)
+    self$fc1 <- nn_linear(
+      in_features = 106,
+      out_features = 64
+    ) # dense in keras = linear in torch
+    self$dropout3 <- nn_dropout(0.1)
+
+    # full model (fc)
+    self$fc2 <- nn_linear(
+      in_features = 64 + 256,                 # CHECK
+      out_features = 256
+    ) 
+    self$fc3 <- nn_linear(
+      in_features = 256,
+      out_features = 1
+    )
+
+  },
+
+  forward = function(data_align, data_pos) {
+
+    #branch 1 (alignment CNN)
+    branch1 <- data_align %>%
+      self$conv1() %>%
+      nnf_relu() %>%
+      nnf_avg_pool1d(kernel_size = 2) %>%   # MISSING IN FLAGEL CODE
+      self$dropout1() %>%                   # MISSING IN FLAGEL CODE
+      self$conv2() %>%
+      nnf_relu() %>%
+      nnf_avg_pool1d(kernel_size = 2) %>%
+      self$dropout2() %>%
+      self$conv3() %>%
+      nnf_relu() %>%
+      nnf_avg_pool1d(kernel_size = 2) %>%
+      self$dropout3() #%>%
+      #torch_flatten()                       # ISSUE??
+
+    # branch 2 (position fc)
+    branch2 <- data_pos %>% 
+      self$fc1() %>% 
+      self$dropout3()
+
+    # full model (fc)
+    full_model <- torch_cat(list(branch1, branch2))
+    
+    full_model %>% 
+      self$fc2() %>% 
+      self$fc3()
+
+  }
 )
 
-genotype_output <-  genotype_input %>%
-  layer_conv_1d(
-    filters = 256,
-    kernel_size = 2,
-    activation = 'relu',
-    kernel_regularizer = regularizer_l2(l2_lambda)
-  ) %>%
-  #layer_average_pooling_1d(pool_size = 2) %>%
-  #layer_dropout(rate = 0.25) %>%
-  layer_conv_1d(
-    filters = 256,
-    kernel_size = 2,
-    activation = 'relu',
-    kernel_regularizer = regularizer_l2(l2_lambda)
-  ) %>%
-  layer_average_pooling_1d(pool_size = 2) %>%
-  layer_dropout(rate = 0.25) %>%
-  layer_conv_1d(
-    filters = 256,
-    kernel_size = 2,
-    activation = 'relu',
-    kernel_regularizer = regularizer_l2(l2_lambda)
-  ) %>%
-  layer_average_pooling_1d(pool_size = 2) %>%
-  layer_dropout(rate = 0.25) %>%
-  layer_flatten()
+model <- flagel_cnn()
 
-position_input <- layer_input(
-  shape = c(106),                # 106 for low, 17 for high
-  dtype = 'float32',
-  name = 'position_input'
-)
+model$forward(low_align_all_train_tensor, low_pos_all_train_tensor)
 
-position_output <- position_input %>%
-  layer_dense(
-    units = 64,
-    activation = 'relu',
-    kernel_regularizer = regularizer_l2(l2_lambda)
-  ) %>%
-  layer_dropout(rate = 0.1)
 
-# merge the inputs, produce one output
-full_output <- layer_concatenate(
-  c(genotype_output, position_output)
-) %>%
-  layer_dense(
-    units = 256,
-    activation = 'relu',
-    kernel_regularizer = regularizer_l2(l2_lambda)
-  ) %>%
-  layer_dense(
-    units = 1,
-    kernel_initializer = 'normal'
-  )
+#--------------- NETWORK PARAMETERS --------------------
 
-# define the full model
-model <- keras_model(
-  inputs = c(genotype_input, position_input),
-  outputs = full_output
-)
 
-summary(model)
-
-#--------------- COMPILE MODEL --------------------
-
-model %>%
-  compile(
-    loss = 'mean_squared_error',
-    #optimizer = 'adam',
-    optimizer = optimizer_adam(lr = 0.000001),       # LOOK
-    metrics = metric_mean_squared_error
-  )
 
 #--------------- TRAIN MODEL --------------------
 
-model %>%
-  fit(
-    x = list(low_align_all_train, low_pos_all_train),
-    y = low_rho_all_train_centered,
-    batch = 32,
-    epochs = 10,  
-    validation_data = list(
-      list(low_align_all_val, low_pos_all_val), 
-      low_rho_all_val_centered
-    )
-  ) -> history
+# forward pass
 
-#plot(history)
+# compute loss
+
+# backpropagation
+
+# update weights
 
 
 #--------------- SAVE MODEL --------------------
 
-# UNCOMMENT AFTER DEBUGGING
-# # save training history 
-# save(
-#   history, 
-#   file = file.path(path_to_results, 'models', 'cnn_model_hist_low_dup_all_60.RData')
-# )
+# save training history
+save(
+  history,
+  #file = file.path(path_to_results, 'models', 'cnn_model_hist_low_dup_all_60.RData')
+)
 
 # save the model 
-
-setwd("/stor/work/Wilke/mmj2238/trained_models/dup_analysis")
-
-# save full model without optimizer
-save_model_hdf5(
-  model, 
-  "cnn_model_full.h5", 
-  overwrite = TRUE, 
-  include_optimizer = FALSE
-)
-
-# save JSON config to disk
-json_config <- model_to_json(model)
-writeLines(
-  json_config, 
-  "cnn_model_config_low_dup_all_10.json"
-)
-
-# save weights to disk
-save_model_weights_hdf5(
-  model, 
-  "cnn_model_weights_low_dup_all_10.h5"
-)
-
-
-
-
-# TEST THAT MODEL SAVES CORRECTLY
-
-# predictions on original model
-model %>% 
-  predict(
-    list(low_align_all_test, low_pos_all_test)
-  ) -> model_predictions
-
-# reload the model from the 2 files we saved
-json_config_2 <- readLines(
-  "cnn_model_config_low_dup_all_60.json"
-)
-
-new_model <- model_from_json(json_config_2)
-load_model_weights_hdf5(
-  new_model, 
-  "cnn_model_weights_low_dup_all_60.h5"
-)
-
-# Check that the state is preserved
-new_predictions <- predict(new_model, list(low_align_all_test, low_pos_all_test))
-all.equal(predictions, new_predictions)
-
-
-# reload the model from the 1 hdf5 file
-new_model_2 <- load_model_hdf5("cnn_model_full.h5", compile = FALSE)
-
-# Check that the state is preserved
-new_predictions_2 <- predict(new_model_2, list(low_align_all_test, low_pos_all_test))
-all.equal(predictions, new_predictions_2)
 
 
 
