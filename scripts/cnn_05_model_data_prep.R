94ae9d5b0a1c96e9a7de8ae0bcdba1e5da075316
mmjohn <mmjohn@utexas.edu>
2021-09-21 17:51:20 -0500

    Prepare full data sets for training

--- scripts/cnn_05_model_data_prep.R
+++ scripts/cnn_05_model_data_prep.R
@@ -3,7 +3,7 @@
 # CNN for recombination rate estimation trained and tested on msprime simulations
 # Using:
 #   2 branch model from Flagel et al. (2018)
-#   60,000 simulations per set
+#   120,000 simulations per set (high/low)
 #   Currently using tensorflow via Keras and reticulate
 # This script: prepares data for input into CNN (transpose, split, remove duplicates, etc)
 # Mackenzie M. Johnson
@@ -24,7 +24,7 @@ devtools::load_all("/stor/home/mmj2238/popgencnn/")
 #--------------- GLOBAL PARAMETERS --------------------
 
 # set the number of simulations in data set
-num_sims <- 60000
+num_sims <- 120000
 
 # paths to data
 path_to_data <- "/stor/work/Wilke/mmj2238/rho_cnn_data/parsed/dup_analysis/cnn_dup/"
@@ -57,101 +57,189 @@ load(file.path(path_to_index, 'high_dup_align_indices.RData'))
 # remove duplicates from sets with them (keep both sets)
 
 # low duplicate data set
-# set n1 without duplicates (set n2 and n3 have no dup)
+# sets 1 and 2 have duplicates, but not 3-6
 pop_low_n1_padded_nodup <- pop_low_n1_padded[-low_n1_index]
-pop_low_n1_pos_nodup <- pop_low_n1_pos_padded[-low_n1_index, 1:106]
+pop_low_n1_pos_nodup <- pop_low_n1_pos_padded[-low_n1_index, 1:174]
 pop_low_n1_rho_nodup <- pop_low_n1_rho[-low_n1_index]
 
-rm(low_n1_index, low_n2_index, low_n3_index)
+pop_low_n2_padded_nodup <- pop_low_n2_padded[-low_n2_index]
+pop_low_n2_pos_nodup <- pop_low_n2_pos_padded[-low_n2_index, 1:174]
+pop_low_n2_rho_nodup <- pop_low_n2_rho[-low_n2_index]
+
+rm(low_n1_index, low_n2_index, low_n3_index,
+   low_n4_index, low_n5_index, low_n6_index)
 
 # high duplicate data set
+# sets 1-6 all have duplicates
 pop_high_n1_padded_nodup <- pop_high_n1_padded[-high_n1_index]
-pop_high_n1_pos_nodup <- pop_high_n1_pos_padded[-high_n1_index, 1:17]
+pop_high_n1_pos_nodup <- pop_high_n1_pos_padded[-high_n1_index, 1:27]
 pop_high_n1_rho_nodup <- pop_high_n1_rho[-high_n1_index]
 
 pop_high_n2_padded_nodup <- pop_high_n2_padded[-high_n2_index]
-pop_high_n2_pos_nodup <- pop_high_n2_pos_padded[-high_n2_index, 1:17]
+pop_high_n2_pos_nodup <- pop_high_n2_pos_padded[-high_n2_index, 1:27]
 pop_high_n2_rho_nodup <- pop_high_n2_rho[-high_n2_index]
 
 pop_high_n3_padded_nodup <- pop_high_n3_padded[-high_n3_index]
-pop_high_n3_pos_nodup <- pop_high_n3_pos_padded[-high_n3_index, 1:17]
+pop_high_n3_pos_nodup <- pop_high_n3_pos_padded[-high_n3_index, 1:27]
 pop_high_n3_rho_nodup <- pop_high_n3_rho[-high_n3_index]
 
-rm(high_n1_index, high_n2_index, high_n3_index)
+pop_high_n4_padded_nodup <- pop_high_n4_padded[-high_n4_index]
+pop_high_n4_pos_nodup <- pop_high_n4_pos_padded[-high_n4_index, 1:27]
+pop_high_n4_rho_nodup <- pop_high_n4_rho[-high_n4_index]
+
+pop_high_n5_padded_nodup <- pop_high_n5_padded[-high_n5_index]
+pop_high_n5_pos_nodup <- pop_high_n5_pos_padded[-high_n5_index, 1:27]
+pop_high_n5_rho_nodup <- pop_high_n5_rho[-high_n5_index]
+
+pop_high_n6_padded_nodup <- pop_high_n6_padded[-high_n6_index]
+pop_high_n6_pos_nodup <- pop_high_n6_pos_padded[-high_n6_index, 1:27]
+pop_high_n6_rho_nodup <- pop_high_n6_rho[-high_n6_index]
+
+rm(high_n1_index, high_n2_index, high_n3_index, high_n4_index,
+   high_n5_index, high_n6_index)
 
 
 #--------------- PREP DATA: JOIN DATA SETS --------------------
 
 # low dup set
 # join aligns - with duplicates
-temp_1 <- append(pop_low_n1_padded, pop_low_n2_padded)
-low_dup_aligns_all <- append(temp_1, pop_low_n3_padded)
+temp_1_1 <- append(pop_low_n1_padded, pop_low_n2_padded)
+temp_1_2 <- append(temp_1_1, pop_low_n3_padded)
+temp_1_3 <- append(temp_1_2, pop_low_n4_padded)
+temp_1_4 <- append(temp_1_3, pop_low_n5_padded)
+low_dup_aligns_all <- append(temp_1_4, pop_low_n6_padded)
+
+rm(temp_1_1, temp_1_2, temp_1_3, temp_1_4)
 
 # join aligns - without duplicates
-temp_2 <- append(pop_low_n1_padded_nodup, pop_low_n2_padded)
-low_dup_aligns_unq <- append(temp_2, pop_low_n3_padded)
+temp_2_1 <- append(pop_low_n1_padded_nodup, pop_low_n2_padded_nodup)
+temp_2_2 <- append(temp_2_1, pop_low_n3_padded)
+temp_2_3 <- append(temp_2_2, pop_low_n4_padded)
+temp_2_4 <- append(temp_2_3, pop_low_n5_padded)
+low_dup_aligns_unq <- append(temp_2_4, pop_low_n6_padded)
 
-rm(temp_1, temp_2, pop_low_n1_padded, pop_low_n1_padded_nodup,
-   pop_low_n2_padded, pop_low_n3_padded)
+rm(temp_2_1, temp_2_2, temp_2_3, temp_2_4)
+rm(pop_low_n1_padded, pop_low_n1_padded_nodup,
+   pop_low_n2_padded, pop_low_n2_padded_nodup, 
+   pop_low_n3_padded, pop_low_n4_padded, 
+   pop_low_n5_padded, pop_low_n6_padded)
 
 # join positions - with duplicates
-temp_3 <- rbind(pop_low_n1_pos_padded, pop_low_n2_pos_padded)
-low_dup_pos_all <- rbind(temp_3, pop_low_n3_pos_padded)
+temp_3_1 <- rbind(pop_low_n1_pos_padded, pop_low_n2_pos_padded)
+temp_3_2 <- rbind(temp_3_1, pop_low_n3_pos_padded)
+temp_3_3 <- rbind(temp_3_2, pop_low_n4_pos_padded)
+temp_3_4 <- rbind(temp_3_3, pop_low_n5_pos_padded)
+low_dup_pos_all <- rbind(temp_3_4, pop_low_n6_pos_padded)
+
+rm(temp_3_1, temp_3_2, temp_3_3, temp_3_4)
 
 # join positions - without duplicates
-temp_4 <- rbind(pop_low_n1_pos_nodup, pop_low_n2_pos_padded)
-low_dup_pos_unq <- rbind(temp_4, pop_low_n3_pos_padded)
+temp_4_1 <- rbind(pop_low_n1_pos_nodup, pop_low_n2_pos_nodup)
+temp_4_2 <- rbind(temp_4_1, pop_low_n3_pos_padded)
+temp_4_3 <- rbind(temp_4_2, pop_low_n4_pos_padded)
+temp_4_4 <- rbind(temp_4_3, pop_low_n5_pos_padded)
+low_dup_pos_unq <- rbind(temp_4_4, pop_low_n6_pos_padded)
 
-rm(temp_3, temp_4, pop_low_n1_pos_nodup, pop_low_n1_pos_padded,
-   pop_low_n2_pos_padded, pop_low_n3_pos_padded)
+rm(temp_4_1, temp_4_2, temp_4_3, temp_4_4)
+rm(pop_low_n1_pos_nodup, pop_low_n1_pos_padded, 
+   pop_low_n2_pos_nodup, pop_low_n2_pos_padded, 
+   pop_low_n3_pos_padded, pop_low_n4_pos_padded,
+   pop_low_n5_pos_padded, pop_low_n6_pos_padded)
 
 # join rho values - with duplicates
-temp_5 <- c(pop_low_n1_rho, pop_low_n2_rho)
-low_dup_rho_all <- c(temp_5, pop_low_n3_rho)
+temp_5_1 <- c(pop_low_n1_rho, pop_low_n2_rho)
+temp_5_2 <- c(temp_5_1, pop_low_n3_rho)
+temp_5_3 <- c(temp_5_2, pop_low_n4_rho)
+temp_5_4 <- c(temp_5_3, pop_low_n5_rho)
+low_dup_rho_all <- c(temp_5_4, pop_low_n6_rho)
+
+rm(temp_5_1, temp_5_2, temp_5_3, temp_5_4)
 
 # join rho values - without duplicates
-temp_6 <- c(pop_low_n1_rho_nodup, pop_low_n2_rho)
-low_dup_rho_unq <- c(temp_6, pop_low_n3_rho)
+temp_6_1 <- c(pop_low_n1_rho_nodup, pop_low_n2_rho_nodup)
+temp_6_2 <- c(temp_6_1, pop_low_n3_rho)
+temp_6_3 <- c(temp_6_2, pop_low_n4_rho)
+temp_6_4 <- c(temp_6_3, pop_low_n5_rho)
+low_dup_rho_unq <- c(temp_6_4, pop_low_n6_rho)
 
-rm(temp_5, temp_6, pop_low_n1_rho, pop_low_n1_rho_nodup, 
-   pop_low_n2_rho, pop_low_n3_rho)
+rm(temp_6_1, temp_6_2, temp_6_3, temp_6_4)
+rm(pop_low_n1_rho, pop_low_n1_rho_nodup, 
+   pop_low_n2_rho, pop_low_n2_rho_nodup, 
+   pop_low_n3_rho, pop_low_n4_rho,
+   pop_low_n5_rho, pop_low_n6_rho)
 
 # high dup set
 # join aligns - with duplicates
-temp_7 <- append(pop_high_n1_padded, pop_high_n2_padded)
-high_dup_aligns_all <- append(temp_7, pop_high_n3_padded)
+temp_7_1 <- append(pop_high_n1_padded, pop_high_n2_padded)
+temp_7_2 <- append(temp_7_1, pop_high_n3_padded)
+temp_7_3 <- append(temp_7_2, pop_high_n4_padded)
+temp_7_4 <- append(temp_7_3, pop_high_n5_padded)
+high_dup_aligns_all <- append(temp_7_4, pop_high_n6_padded)
+
+rm(temp_7_1, temp_7_2, temp_7_3, temp_7_4)
 
 # join aligns - without duplicates
-temp_8 <- append(pop_high_n1_padded_nodup, pop_high_n2_padded_nodup)
-high_dup_aligns_unq <- append(temp_8, pop_high_n3_padded_nodup)
+temp_8_1 <- append(pop_high_n1_padded_nodup, pop_high_n2_padded_nodup)
+temp_8_2 <- append(temp_8_1, pop_high_n3_padded_nodup)
+temp_8_3 <- append(temp_8_2, pop_high_n4_padded_nodup)
+temp_8_4 <- append(temp_8_3, pop_high_n5_padded_nodup)
+high_dup_aligns_unq <- append(temp_8_4, pop_high_n6_padded_nodup)
 
-rm(temp_7, temp_8, pop_high_n1_padded, pop_high_n2_padded,
-   pop_high_n3_padded, pop_high_n1_padded_nodup,
-   pop_high_n2_padded_nodup, pop_high_n3_padded_nodup)
+rm(temp_8_1, temp_8_2, temp_8_3, temp_8_4)
+rm(pop_high_n1_padded, pop_high_n2_padded,
+   pop_high_n3_padded, pop_high_n4_padded,
+   pop_high_n5_padded, pop_high_n6_padded, 
+   pop_high_n1_padded_nodup, pop_high_n2_padded_nodup, 
+   pop_high_n3_padded_nodup, pop_high_n4_padded_nodup,
+   pop_high_n5_padded_nodup, pop_high_n6_padded_nodup)
 
 # join positions - with duplicates
-temp_9 <- rbind(pop_high_n1_pos_padded, pop_high_n2_pos_padded)
-high_dup_pos_all <- rbind(temp_9, pop_high_n3_pos_padded)
+temp_9_1 <- rbind(pop_high_n1_pos_padded, pop_high_n2_pos_padded)
+temp_9_2 <- rbind(temp_9_1, pop_high_n3_pos_padded)
+temp_9_3 <- rbind(temp_9_2, pop_high_n4_pos_padded)
+temp_9_4 <- rbind(temp_9_3, pop_high_n5_pos_padded)
+high_dup_pos_all <- rbind(temp_9_4, pop_high_n6_pos_padded)
+
+rm(temp_9_1, temp_9_2, temp_9_3, temp_9_4)
 
 # join positions - without duplicates
-temp_10 <- rbind(pop_high_n1_pos_nodup, pop_high_n2_pos_nodup)
-high_dup_pos_unq <- rbind(temp_10, pop_high_n3_pos_nodup)
+temp_10_1 <- rbind(pop_high_n1_pos_nodup, pop_high_n2_pos_nodup)
+temp_10_2 <- rbind(temp_10_1, pop_high_n3_pos_nodup)
+temp_10_3 <- rbind(temp_10_2, pop_high_n4_pos_nodup)
+temp_10_4 <- rbind(temp_10_3, pop_high_n5_pos_nodup)
+high_dup_pos_unq <- rbind(temp_10_4, pop_high_n6_pos_nodup)
 
-rm(temp_9, temp_10, pop_high_n1_pos_padded, pop_high_n2_pos_padded,
-   pop_high_n3_pos_padded, pop_high_n1_pos_nodup, 
-   pop_high_n2_pos_nodup, pop_high_n3_pos_nodup)
+rm(temp_10_1, temp_10_2, temp_10_3, temp_10_4)
+rm(pop_high_n1_pos_padded, pop_high_n2_pos_padded,
+   pop_high_n3_pos_padded, pop_high_n4_pos_padded,
+   pop_high_n5_pos_padded, pop_high_n6_pos_padded, 
+   pop_high_n1_pos_nodup, pop_high_n2_pos_nodup, 
+   pop_high_n3_pos_nodup, pop_high_n4_pos_nodup,
+   pop_high_n5_pos_nodup, pop_high_n6_pos_nodup)
 
 # join rho values - with duplicates
-temp_11 <- c(pop_high_n1_rho, pop_high_n2_rho)
-high_dup_rho_all <- c(temp_11, pop_high_n3_rho)
+temp_11_1 <- c(pop_high_n1_rho, pop_high_n2_rho)
+temp_11_2 <- c(temp_11_1, pop_high_n3_rho)
+temp_11_3 <- c(temp_11_2, pop_high_n4_rho)
+temp_11_4 <- c(temp_11_3, pop_high_n5_rho)
+high_dup_rho_all <- c(temp_11_4, pop_high_n6_rho)
+
+rm(temp_11_1, temp_11_2, temp_11_3, temp_11_4)
 
 # join rho values - without duplicates
-temp_12 <- c(pop_high_n1_rho_nodup, pop_high_n2_rho_nodup)
-high_dup_rho_unq <- c(temp_12, pop_high_n3_rho_nodup)
+temp_12_1 <- c(pop_high_n1_rho_nodup, pop_high_n2_rho_nodup)
+temp_12_2 <- c(temp_12_1, pop_high_n3_rho_nodup)
+temp_12_3 <- c(temp_12_2, pop_high_n4_rho_nodup)
+temp_12_4 <- c(temp_12_3, pop_high_n5_rho_nodup)
+high_dup_rho_unq <- c(temp_12_4, pop_high_n6_rho_nodup)
 
-rm(temp_11, temp_12, pop_high_n1_rho, pop_high_n2_rho,
-   pop_high_n3_rho, pop_high_n1_rho_nodup, 
-   pop_high_n2_rho_nodup, pop_high_n3_rho_nodup)
+rm(temp_12_1, temp_12_2, temp_12_3, temp_12_4)
+rm(pop_high_n1_rho, pop_high_n2_rho,
+   pop_high_n3_rho, pop_high_n4_rho,
+   pop_high_n5_rho, pop_high_n6_rho,
+   pop_high_n1_rho_nodup, pop_high_n2_rho_nodup, 
+   pop_high_n3_rho_nodup, pop_high_n4_rho_nodup,
+   pop_high_n5_rho_nodup, pop_high_n6_rho_nodup)
 
 
 #--------------- PREP DATA: CONVERT ALIGNMENTS --------------------
@@ -160,7 +248,7 @@ rm(temp_11, temp_12, pop_high_n1_rho, pop_high_n2_rho,
 # low dup set
 low_dup_all_array <- popgencnn::list_to_array(
   low_dup_aligns_all, 
-  dim = c(50, 106, 60000)
+  dim = c(50, 174, 120000)
 )
 
 low_dup_all_align <- aperm(
@@ -172,7 +260,7 @@ rm(low_dup_aligns_all, low_dup_all_array)
 
 low_dup_unq_array <- popgencnn::list_to_array(
   low_dup_aligns_unq, 
-  dim = c(50, 106, 58532)
+  dim = c(50, 174, 118512)
 )
 
 low_dup_unq_align <- aperm(
@@ -185,7 +273,7 @@ rm(low_dup_aligns_unq, low_dup_unq_array)
 # high dup set
 high_dup_all_array <- popgencnn::list_to_array(
   high_dup_aligns_all, 
-  dim = c(50, 17, 60000)
+  dim = c(50, 27, 120000)
 )
 
 high_dup_all_align <- aperm(
@@ -197,7 +285,7 @@ rm(high_dup_aligns_all, high_dup_all_array)
 
 high_dup_unq_array <- popgencnn::list_to_array(
   high_dup_aligns_unq, 
-  dim = c(50, 17, 32714)
+  dim = c(50, 27, 76645)
 )
 
 high_dup_unq_align <- aperm(
@@ -213,93 +301,93 @@ rm(high_dup_aligns_unq, high_dup_unq_array)
 # split data into training, validation, and test set randomly
 # get number for each set
 # low dup set
-length(low_dup_rho_all)*0.6   # 36,000 for training
-length(low_dup_rho_all)*0.2   # 12,000 for validation
-length(low_dup_rho_all)*0.2   # 12,000 for testing
+length(low_dup_rho_all)*0.6   # 72,000 for training
+length(low_dup_rho_all)*0.2   # 24,000 for validation
+length(low_dup_rho_all)*0.2   # 24,000 for testing
 
-length(low_dup_rho_unq)*0.6   # 35,120 for training
-length(low_dup_rho_unq)*0.2   # 11,706 for validation
-length(low_dup_rho_unq)*0.2   # 11,706 for testing
+length(low_dup_rho_unq)*0.6   # 71,108 for training
+length(low_dup_rho_unq)*0.2   # 23,702 for validation
+length(low_dup_rho_unq)*0.2   # 23,702 for testing
 
 # high dup set
-length(high_dup_rho_all)*0.6   # 36,000 for training
-length(high_dup_rho_all)*0.2   # 12,000 for validation
-length(high_dup_rho_all)*0.2   # 12,000 for testing
+length(high_dup_rho_all)*0.6   # 72,000 for training
+length(high_dup_rho_all)*0.2   # 24,000 for validation
+length(high_dup_rho_all)*0.2   # 24,000 for testing
 
-length(high_dup_rho_unq)*0.6   # 19,628 for training
-length(high_dup_rho_unq)*0.2   # 6,543 for validation
-length(high_dup_rho_unq)*0.2   # 6,543 for testing
+length(high_dup_rho_unq)*0.6   # 45,987 for training
+length(high_dup_rho_unq)*0.2   # 15,329 for validation
+length(high_dup_rho_unq)*0.2   # 15,329 for testing
 
 # randomly select indices for each set 
-all_index <- 1:60000
-unq_index <- 1:58532
-h_unq_index <- 1:32714
+all_index <- 1:120000
+l_unq_index <- 1:118512
+h_unq_index <- 1:76645
 
 sampled_all_index <- split(
   all_index, 
-  sample(rep(1:3, c(36000, 12000, 12000)))
+  sample(rep(1:3, c(72000, 24000, 24000)))
 )
 sampled_unq_index <- split(
-  unq_index, 
-  sample(rep(1:3, c(35120, 11706, 11706)))
+  l_unq_index, 
+  sample(rep(1:3, c(71108, 23702, 23702)))
 )
 
 h_sampled_all_index <- split(
   all_index, 
-  sample(rep(1:3, c(36000, 12000, 12000)))
+  sample(rep(1:3, c(72000, 24000, 24000)))
 )
 h_sampled_unq_index <- split(
   h_unq_index, 
-  sample(rep(1:3, c(19628, 6543, 6543)))
+  sample(rep(1:3, c(45987, 15329, 15329)))
 )
 
 # split into sets based on index
 # low dup set
-low_align_all_train <- low_dup_all_align[sampled_all_index[[1]], 1:106, 1:50]
-low_align_unq_train <- low_dup_unq_align[sampled_unq_index[[1]], 1:106, 1:50]
-low_pos_all_train <- low_dup_pos_all[sampled_all_index[[1]], 1:106]
-low_pos_unq_train <- low_dup_pos_unq[sampled_unq_index[[1]], 1:106]
+low_align_all_train <- low_dup_all_align[sampled_all_index[[1]], 1:174, 1:50]
+low_align_unq_train <- low_dup_unq_align[sampled_unq_index[[1]], 1:174, 1:50]
+low_pos_all_train <- low_dup_pos_all[sampled_all_index[[1]], 1:174]
+low_pos_unq_train <- low_dup_pos_unq[sampled_unq_index[[1]], 1:174]
 low_rho_all_train <- low_dup_rho_all[sampled_all_index[[1]]]
 low_rho_unq_train <- low_dup_rho_unq[sampled_unq_index[[1]]]
 
-low_align_all_val <- low_dup_all_align[sampled_all_index[[2]], 1:106, 1:50]
-low_align_unq_val <- low_dup_unq_align[sampled_unq_index[[2]], 1:106, 1:50]
-low_pos_all_val <- low_dup_pos_all[sampled_all_index[[2]], 1:106]
-low_pos_unq_val <- low_dup_pos_unq[sampled_unq_index[[2]], 1:106]
+low_align_all_val <- low_dup_all_align[sampled_all_index[[2]], 1:174, 1:50]
+low_align_unq_val <- low_dup_unq_align[sampled_unq_index[[2]], 1:174, 1:50]
+low_pos_all_val <- low_dup_pos_all[sampled_all_index[[2]], 1:174]
+low_pos_unq_val <- low_dup_pos_unq[sampled_unq_index[[2]], 1:174]
 low_rho_all_val <- low_dup_rho_all[sampled_all_index[[2]]]
 low_rho_unq_val <- low_dup_pos_unq[sampled_unq_index[[2]]]
 
-low_align_all_test <- low_dup_all_align[sampled_all_index[[3]], 1:106, 1:50]
-low_align_unq_test <- low_dup_unq_align[sampled_unq_index[[3]], 1:106, 1:50]
-low_pos_all_test <- low_dup_pos_all[sampled_all_index[[3]], 1:106]
-low_pos_unq_test <- low_dup_pos_unq[sampled_unq_index[[3]], 1:106]
+low_align_all_test <- low_dup_all_align[sampled_all_index[[3]], 1:174, 1:50]
+low_align_unq_test <- low_dup_unq_align[sampled_unq_index[[3]], 1:174, 1:50]
+low_pos_all_test <- low_dup_pos_all[sampled_all_index[[3]], 1:174]
+low_pos_unq_test <- low_dup_pos_unq[sampled_unq_index[[3]], 1:174]
 low_rho_all_test <- low_dup_rho_all[sampled_all_index[[3]]]
 low_rho_unq_test <- low_dup_rho_unq[sampled_unq_index[[3]]]
 
 rm(low_dup_all_align, low_dup_pos_all, low_dup_rho_all,
    all_index, sampled_all_index)
 rm(low_dup_unq_align, low_dup_pos_unq, low_dup_rho_unq, 
-   unq_index, sampled_unq_index)
+   l_unq_index, sampled_unq_index)
 
 # high dup set
-high_align_all_train <- high_dup_all_align[h_sampled_all_index[[1]], 1:17, 1:50]
-high_align_unq_train <- high_dup_unq_align[h_sampled_unq_index[[1]], 1:17, 1:50]
-high_pos_all_train <- high_dup_pos_all[h_sampled_all_index[[1]], 1:17]
-high_pos_unq_train <- high_dup_pos_unq[h_sampled_unq_index[[1]], 1:17]
+high_align_all_train <- high_dup_all_align[h_sampled_all_index[[1]], 1:27, 1:50]
+high_align_unq_train <- high_dup_unq_align[h_sampled_unq_index[[1]], 1:27, 1:50]
+high_pos_all_train <- high_dup_pos_all[h_sampled_all_index[[1]], 1:27]
+high_pos_unq_train <- high_dup_pos_unq[h_sampled_unq_index[[1]], 1:27]
 high_rho_all_train <- high_dup_rho_all[h_sampled_all_index[[1]]]
 high_rho_unq_train <- high_dup_rho_unq[h_sampled_unq_index[[1]]]
 
-high_align_all_val <- high_dup_all_align[h_sampled_all_index[[2]], 1:17, 1:50]
-high_align_unq_val <- high_dup_unq_align[h_sampled_unq_index[[2]], 1:17, 1:50]
-high_pos_all_val <- high_dup_pos_all[h_sampled_all_index[[2]], 1:17]
-high_pos_unq_val <- high_dup_pos_unq[h_sampled_unq_index[[2]], 1:17]
+high_align_all_val <- high_dup_all_align[h_sampled_all_index[[2]], 1:27, 1:50]
+high_align_unq_val <- high_dup_unq_align[h_sampled_unq_index[[2]], 1:27, 1:50]
+high_pos_all_val <- high_dup_pos_all[h_sampled_all_index[[2]], 1:27]
+high_pos_unq_val <- high_dup_pos_unq[h_sampled_unq_index[[2]], 1:27]
 high_rho_all_val <- high_dup_rho_all[h_sampled_all_index[[2]]]
 high_rho_unq_val <- high_dup_rho_unq[h_sampled_unq_index[[2]]]
 
-high_align_all_test <- high_dup_all_align[h_sampled_all_index[[3]], 1:17, 1:50]
-high_align_unq_test <- high_dup_unq_align[h_sampled_unq_index[[3]], 1:17, 1:50]
-high_pos_all_test <- high_dup_pos_all[h_sampled_all_index[[3]], 1:17]
-high_pos_unq_test <- high_dup_pos_unq[h_sampled_unq_index[[3]], 1:17]
+high_align_all_test <- high_dup_all_align[h_sampled_all_index[[3]], 1:27, 1:50]
+high_align_unq_test <- high_dup_unq_align[h_sampled_unq_index[[3]], 1:27, 1:50]
+high_pos_all_test <- high_dup_pos_all[h_sampled_all_index[[3]], 1:27]
+high_pos_unq_test <- high_dup_pos_unq[h_sampled_unq_index[[3]], 1:27]
 high_rho_all_test <- high_dup_rho_all[h_sampled_all_index[[3]]]
 high_rho_unq_test <- high_dup_rho_unq[h_sampled_unq_index[[3]]]
 
@@ -374,7 +462,8 @@ save(
   low_align_all_train, low_align_all_val, low_align_all_test,
   low_pos_all_train, low_pos_all_val, low_pos_all_test,
   low_rho_all_train, low_rho_all_val, low_rho_all_test,
-  low_rho_all_train_centered, low_rho_all_val_centered, low_rho_all_test_centered, 
+  low_rho_all_train_centered, low_rho_all_val_centered, 
+  low_rho_all_test_centered, 
   file = file.path(
     path_to_data, 
     'model_data_low_dup_all.RData')
@@ -385,7 +474,8 @@ save(
   low_align_unq_train, low_align_unq_val, low_align_unq_test,
   low_pos_unq_train, low_pos_unq_val, low_pos_unq_test,
   low_rho_unq_train, low_rho_unq_val, low_rho_unq_test,
-  low_rho_unq_train_centered, low_rho_unq_val_centered, low_rho_unq_test_centered, 
+  low_rho_unq_train_centered, low_rho_unq_val_centered, 
+  low_rho_unq_test_centered, 
   file = file.path(
     path_to_data, 
     'model_data_low_dup_unq.RData')
