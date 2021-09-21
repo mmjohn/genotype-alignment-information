2f603d92e260dd7e8172c2125107a858b991e9e6
mmjohn <mmjohn@utexas.edu>
2021-09-21 13:46:26 -0500

    Parse full data set

--- scripts/cnn_02_parse_data.R
+++ scripts/cnn_02_parse_data.R
@@ -38,10 +38,19 @@ pop_low_n1 <- readLines(
   file.path(path_to_raw, 'cnn_dup', 'low_dup_n_1000_sims_all.txt')
 )
 pop_low_n2 <- readLines(
-  file.path(path_to_raw, 'cnn_dup', 'low_dup_n_5000_sims_all.txt')
+  file.path(path_to_raw, 'cnn_dup', 'low_dup_n_2000_sims_all.txt')
 )
 pop_low_n3 <- readLines(
+  file.path(path_to_raw, 'cnn_dup', 'low_dup_n_5000_sims_all.txt')
+)
+pop_low_n4 <- readLines(
   file.path(path_to_raw, 'cnn_dup', 'low_dup_n_10000_sims_all.txt')
+)
+pop_low_n5 <- readLines(
+  file.path(path_to_raw, 'cnn_dup', 'low_dup_n_15000_sims_all.txt')
+)
+pop_low_n6 <- readLines(
+  file.path(path_to_raw, 'cnn_dup', 'low_dup_n_20000_sims_all.txt')
 )
 
 
@@ -50,10 +59,19 @@ pop_high_n1 <- readLines(
   file.path(path_to_raw, 'cnn_dup', 'high_dup_n_1000_sims_all.txt')
 )
 pop_high_n2 <- readLines(
-  file.path(path_to_raw, 'cnn_dup', 'high_dup_n_5000_sims_all.txt')
+  file.path(path_to_raw, 'cnn_dup', 'high_dup_n_2000_sims_all.txt')
 )
 pop_high_n3 <- readLines(
+  file.path(path_to_raw, 'cnn_dup', 'high_dup_n_5000_sims_all.txt')
+)
+pop_high_n4 <- readLines(
   file.path(path_to_raw, 'cnn_dup', 'high_dup_n_10000_sims_all.txt')
+)
+pop_high_n5 <- readLines(
+  file.path(path_to_raw, 'cnn_dup', 'high_dup_n_15000_sims_all.txt')
+)
+pop_high_n6 <- readLines(
+  file.path(path_to_raw, 'cnn_dup', 'high_dup_n_20000_sims_all.txt')
 )
 
 
@@ -63,26 +81,36 @@ pop_high_n3 <- readLines(
 pop_low_n1_unsorted <- popgencnn::get_alignment_data(pop_low_n1)
 pop_low_n2_unsorted <- popgencnn::get_alignment_data(pop_low_n2)
 pop_low_n3_unsorted <- popgencnn::get_alignment_data(pop_low_n3)
+pop_low_n4_unsorted <- popgencnn::get_alignment_data(pop_low_n4)
+pop_low_n5_unsorted <- popgencnn::get_alignment_data(pop_low_n5)
+pop_low_n6_unsorted <- popgencnn::get_alignment_data(pop_low_n6)
 
 # parse alignments: high proportion duplicates
 pop_high_n1_unsorted <- popgencnn::get_alignment_data(pop_high_n1)
 pop_high_n2_unsorted <- popgencnn::get_alignment_data(pop_high_n2)
 pop_high_n3_unsorted <- popgencnn::get_alignment_data(pop_high_n3)
+pop_high_n4_unsorted <- popgencnn::get_alignment_data(pop_high_n4)
+pop_high_n5_unsorted <- popgencnn::get_alignment_data(pop_high_n5)
+pop_high_n6_unsorted <- popgencnn::get_alignment_data(pop_high_n6)
 
 # save alignment data
 save(
   pop_low_n1_unsorted, pop_low_n2_unsorted, pop_low_n3_unsorted,
+  pop_low_n4_unsorted, pop_low_n5_unsorted, pop_low_n6_unsorted,
   file = file.path(path_to_parsed, 'cnn_dup', 'low_dup_align.RData')
 )
 
 save(
   pop_high_n1_unsorted, pop_high_n2_unsorted, pop_high_n3_unsorted, 
+  pop_high_n4_unsorted, pop_high_n5_unsorted, pop_high_n6_unsorted, 
   file = file.path(path_to_parsed, 'cnn_dup', 'high_dup_align.RData')
 )
 
 # remove alignments
-rm(pop_low_n1_unsorted, pop_low_n2_unsorted, pop_low_n3_unsorted)
-rm(pop_high_n1_unsorted, pop_high_n2_unsorted, pop_high_n3_unsorted)
+rm(pop_low_n1_unsorted, pop_low_n2_unsorted, pop_low_n3_unsorted,
+   pop_low_n4_unsorted, pop_low_n5_unsorted, pop_low_n6_unsorted)
+rm(pop_high_n1_unsorted, pop_high_n2_unsorted, pop_high_n3_unsorted, 
+   pop_high_n4_unsorted, pop_high_n5_unsorted, pop_high_n6_unsorted)
 
 
 #--------------- PARSE DATA: RHOS --------------------
@@ -118,26 +146,36 @@ get_rho_msp <- function(all_data) {
 pop_low_n1_rho <- get_rho_msp(pop_low_n1)
 pop_low_n2_rho <- get_rho_msp(pop_low_n2)
 pop_low_n3_rho <- get_rho_msp(pop_low_n3)
+pop_low_n4_rho <- get_rho_msp(pop_low_n4)
+pop_low_n5_rho <- get_rho_msp(pop_low_n5)
+pop_low_n6_rho <- get_rho_msp(pop_low_n6)
 
 # high dup set
 pop_high_n1_rho <- get_rho_msp(pop_high_n1)
 pop_high_n2_rho <- get_rho_msp(pop_high_n2)
 pop_high_n3_rho <- get_rho_msp(pop_high_n3)
+pop_high_n4_rho <- get_rho_msp(pop_high_n4)
+pop_high_n5_rho <- get_rho_msp(pop_high_n5)
+pop_high_n6_rho <- get_rho_msp(pop_high_n6)
 
 # save rho data
 save(
   pop_low_n1_rho, pop_low_n2_rho, pop_low_n3_rho,
+  pop_low_n4_rho, pop_low_n5_rho, pop_low_n6_rho,
   file = file.path(path_to_parsed, 'cnn_dup', 'low_dup_rho.RData')
 )
 
 save(
   pop_high_n1_rho, pop_high_n2_rho, pop_high_n3_rho,
+  pop_high_n4_rho, pop_high_n5_rho, pop_high_n6_rho,
   file = file.path(path_to_parsed, 'cnn_dup', 'high_dup_rho.RData')
 )
 
 # remove rho data
-rm(pop_low_n1_rho, pop_low_n2_rho, pop_low_n3_rho)
-rm(pop_high_n1_rho, pop_high_n2_rho, pop_high_n3_rho)
+rm(pop_low_n1_rho, pop_low_n2_rho, pop_low_n3_rho,
+   pop_low_n4_rho, pop_low_n5_rho, pop_low_n6_rho)
+rm(pop_high_n1_rho, pop_high_n2_rho, pop_high_n3_rho,
+   pop_high_n4_rho, pop_high_n5_rho, pop_high_n6_rho)
 
 
 #--------------- PARSE DATA: SEG SITES (#) --------------------
@@ -164,26 +202,36 @@ get_seg_sites <- function(all_data){
 pop_low_n1_sites <- get_seg_sites(pop_low_n1)
 pop_low_n2_sites <- get_seg_sites(pop_low_n2)
 pop_low_n3_sites <- get_seg_sites(pop_low_n3)
+pop_low_n4_sites <- get_seg_sites(pop_low_n4)
+pop_low_n5_sites <- get_seg_sites(pop_low_n5)
+pop_low_n6_sites <- get_seg_sites(pop_low_n6)
 
 # high dup set
 pop_high_n1_sites <- get_seg_sites(pop_high_n1)
 pop_high_n2_sites <- get_seg_sites(pop_high_n2)
 pop_high_n3_sites <- get_seg_sites(pop_high_n3)
+pop_high_n4_sites <- get_seg_sites(pop_high_n4)
+pop_high_n5_sites <- get_seg_sites(pop_high_n5)
+pop_high_n6_sites <- get_seg_sites(pop_high_n6)
 
 # save seg sites data
 save(
   pop_low_n1_sites, pop_low_n2_sites, pop_low_n3_sites,
+  pop_low_n4_sites, pop_low_n5_sites, pop_low_n6_sites,
   file = file.path(path_to_parsed, 'cnn_dup', 'low_dup_sites.RData')
 )
 
 save(
   pop_high_n1_sites, pop_high_n2_sites, pop_high_n3_sites,
+  pop_high_n4_sites, pop_high_n5_sites, pop_high_n6_sites,
   file = file.path(path_to_parsed, 'cnn_dup', 'high_dup_sites.RData')
 )
 
 # remove data 
-rm(pop_low_n1_sites, pop_low_n2_sites, pop_low_n3_sites)
-rm(pop_high_n1_sites, pop_high_n2_sites, pop_high_n3_sites)
+rm(pop_low_n1_sites, pop_low_n2_sites, pop_low_n3_sites,
+   pop_low_n4_sites, pop_low_n5_sites, pop_low_n6_sites)
+rm(pop_high_n1_sites, pop_high_n2_sites, pop_high_n3_sites,
+   pop_high_n4_sites, pop_high_n5_sites, pop_high_n6_sites)
 
 
 #--------------- PARSE DATA: SEG SITES (POS) --------------------
@@ -193,28 +241,38 @@ rm(pop_high_n1_sites, pop_high_n2_sites, pop_high_n3_sites)
 pop_low_n1_pos <- popgencnn::get_pos_data(pop_low_n1)
 pop_low_n2_pos <- popgencnn::get_pos_data(pop_low_n2)
 pop_low_n3_pos <- popgencnn::get_pos_data(pop_low_n3)
+pop_low_n4_pos <- popgencnn::get_pos_data(pop_low_n4)
+pop_low_n5_pos <- popgencnn::get_pos_data(pop_low_n5)
+pop_low_n6_pos <- popgencnn::get_pos_data(pop_low_n6)
 
 # high dup set
 pop_high_n1_pos <- popgencnn::get_pos_data(pop_high_n1)
 pop_high_n2_pos <- popgencnn::get_pos_data(pop_high_n2)
 pop_high_n3_pos <- popgencnn::get_pos_data(pop_high_n3)
+pop_high_n4_pos <- popgencnn::get_pos_data(pop_high_n4)
+pop_high_n5_pos <- popgencnn::get_pos_data(pop_high_n5)
+pop_high_n6_pos <- popgencnn::get_pos_data(pop_high_n6)
 
 # save position data
 save(
   pop_low_n1_pos, pop_low_n2_pos, pop_low_n3_pos,
+  pop_low_n4_pos, pop_low_n5_pos, pop_low_n6_pos,
   file = file.path(path_to_parsed, 'cnn_dup', 'low_dup_pos.RData')
 )
 
 save(
   pop_high_n1_pos, pop_high_n2_pos, pop_high_n3_pos,
+  pop_high_n4_pos, pop_high_n5_pos, pop_high_n6_pos,
   file = file.path(path_to_parsed, 'cnn_dup', 'high_dup_pos.RData')
 )
 
 # remove data
-rm(pop_low_n1_pos, pop_low_n2_pos, pop_low_n3_pos)
-rm(pop_high_n1_pos, pop_high_n2_pos, pop_high_n3_pos)
+rm(pop_low_n1_pos, pop_low_n2_pos, pop_low_n3_pos,
+   pop_low_n4_pos, pop_low_n5_pos, pop_low_n6_pos)
+rm(pop_high_n1_pos, pop_high_n2_pos, pop_high_n3_pos,
+   pop_high_n4_pos, pop_high_n5_pos, pop_high_n6_pos)
 
-rm(pop_low_n1, pop_low_n2, pop_low_n3)
-rm(pop_high_n1, pop_high_n2, pop_high_n3)
+rm(pop_low_n1, pop_low_n2, pop_low_n3, pop_low_n4, pop_low_n5, pop_low_n6)
+rm(pop_high_n1, pop_high_n2, pop_high_n3, pop_high_n4, pop_high_n5, pop_high_n6)
 
 
