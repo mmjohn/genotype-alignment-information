ebeae19155582744eb7c402b44a6df414ede3635
mmjohn <mmjohn@utexas.edu>
2022-05-10 20:46:58 -0500

    Increase font size, add labels, and change color palette for fig_rho_site

--- results/dup_parameter_rho_figs.R
+++ results/dup_parameter_rho_figs.R
@@ -87,18 +87,7 @@ rho_mu_df %>%
 fig_rho_n
 
 
-
 #--------------- FIXED MU RHO DISTRIBUTION FIGURES --------------------
-
-# combine data sets for easy facet
-rho_mu_df %>% 
-  mutate(processing = "Sorted") -> rho_mu_df
-
-# rho_mu_unsort_df %>% 
-#   mutate(processing = "Unsorted") -> rho_mu_unsort_df
-# 
-# rho_mu_full <- full_join(rho_mu_df, rho_mu_unsort_df) 
-# rho_mu_full$processing <- as.factor(rho_mu_full$processing)
 
 # manually set labels
 labels_n <- c(
@@ -111,7 +100,6 @@ labels_n <- c(
 rho_mu_df %>% 
   filter(mut_rate == 1.5e-8) %>% 
   filter(pop_size == 100 | pop_size == 316 | pop_size == 1000) %>% 
-  mutate(processing = factor(processing, levels = c("Unsorted", "Sorted"))) %>% 
   ggplot(
     aes(x = rho, fill = status)
   ) +
@@ -136,11 +124,11 @@ rho_mu_df %>%
     name = "Density"
   ) +
   scale_fill_discrete_sequential(
-    palette = "Purp",
+    palette = "Burg",
     name = "Identity",
     labels = c("Duplicate", "Unique")
   ) +
-  theme_linedraw(12) +
+  theme_linedraw(16) + #12
   theme(
     axis.title.x = element_markdown(),
     panel.grid.major.x = element_blank(),
@@ -153,78 +141,12 @@ rho_mu_df %>%
 fig_rho_dup_v_unq_fixed_mu
 
 
-# #--------------- FIXED N RHO DISTRIBUTION FIGURES --------------------
-# 
-# # combine data sets for easy facet
-# rho_n_df %>% 
-#   mutate(processing = "Sorted") -> rho_n_df
-# 
-# rho_n_unsort_df %>% 
-#   mutate(processing = "Unsorted") -> rho_n_unsort_df
-# 
-# rho_n_full <- full_join(rho_n_df, rho_n_unsort_df) 
-# rho_n_full$processing <- as.factor(rho_n_full$processing)
-# 
-# # manually set labels
-# labels_mu <- c(
-#   "1e-10" = "&mu; = 1.0e-10", 
-#   "3.16e-10" = "&mu; = 3.16e-10",
-#   "1e-09" = "&mu; = 1.0e-9"
-# )
-# 
-# # figure showing rho values of duplicates compared to whole data set
-# rho_n_full %>% 
-#   filter(pop_size == 10000) %>% 
-#   filter(mut_rate == 1.00e-10 | mut_rate == 3.16e-10 | mut_rate == 1.00e-09) %>% 
-#   mutate(processing = factor(processing, levels = c("Unsorted", "Sorted"))) %>% 
-#   ggplot(
-#     aes(x = rho, fill = status)
-#   ) +
-#   geom_density(
-#     aes(y = after_stat(count)), 
-#     alpha = 0.7,
-#     position = "fill"
-#   ) +
-#   facet_grid(
-#     rows = vars(processing),
-#     cols = vars(mut_rate),
-#     scales = "free_x",
-#     labeller = labeller(mut_rate = labels_mu)
-#   ) +
-#   scale_x_continuous(
-#     expand = c(0 ,0),
-#     name = "&rho;"
-#   ) +
-#   scale_y_continuous(
-#     breaks = c(0.25, 0.5, 0.75, 1),
-#     expand = c(0, 0),
-#     name = "Density"
-#   ) +
-#   scale_fill_discrete_sequential(
-#     palette = "Purp",
-#     name = "Identity",
-#     labels = c("Duplicate", "Unique")
-#   ) +
-#   theme_linedraw(12) +
-#   theme(
-#     axis.title.x = element_markdown(),
-#     panel.grid.major.x = element_blank(),
-#     panel.grid.minor.x = element_blank(),
-#     strip.background = element_rect(fill = "grey92"),
-#     strip.text = element_markdown(color = "black")
-#   ) -> fig_rho_dup_v_unq_fixed_n
-# 
-# fig_rho_dup_v_unq_fixed_n
-
-
-
 #--------------- FIXED MU SITES DISTRIBUTION FIGURES --------------------
 
 # figure showing seg. sites of duplicates compared to whole data set
 sites_mu_df %>% 
   filter(mut_rate == 1.5e-8) %>% 
   filter(pop_size == 100 | pop_size == 316 | pop_size == 1000) %>% 
-  #mutate(processing = factor(processing, levels = c("Unsorted", "Sorted"))) %>% 
   ggplot(aes(x = segsites, fill = status)) +
   geom_bar(position = "stack") +
   facet_grid(
@@ -233,18 +155,21 @@ sites_mu_df %>%
     labeller = labeller(pop_size = labels_n)
   ) +
   scale_fill_discrete_sequential(
-    palette = "Purp"
+    palette = "Burg" #"Purp"
   ) +
+  # scale_fill_discrete_qualitative(
+  #   palette = "Dark 2"
+  # ) +
   coord_cartesian(xlim = c(-0.2,10)) +
   scale_x_continuous(
     breaks = c(0, 2, 4, 6, 8, 10),
     name = "Number of segregating sites"
   ) +
   scale_y_continuous(
-    expand = expansion(mult = c(0, .05)), 
+    expand = expansion(mult = c(0, .05)),
     name = "Count"
   ) +
-  theme_bw(12) +
+  theme_bw(16) + # 12
   theme(
     strip.text = element_markdown(),
     legend.position = "none"
@@ -255,10 +180,7 @@ fig_sites_sort_v_unsort_fixed_mu
 
 #--------------- SAVE FIGURES --------------------
 
-fig_rho_site <- plot_grid(
-  fig_rho_dup_v_unq_fixed_mu, fig_sites_sort_v_unsort_fixed_mu,
-  nrow = 2
-)
+fig_rho_dup_v_unq_fixed_mu / fig_sites_sort_v_unsort_fixed_mu
 
 fig_rho_site <- fig_rho_dup_v_unq_fixed_mu / fig_sites_sort_v_unsort_fixed_mu
 
@@ -278,17 +200,4 @@ save_plot(
   base_asp = 1.618, base_width = NULL
 )
 
-# save_plot( 
-#   file.path(path_to_results, 'figures', 'fig_rho_dup_fixed_mu.png'),
-#   fig_rho_dup_v_unq_fixed_mu, 
-#   ncol = 1, nrow = 1, base_height = 3.71,
-#   base_asp = 1.618, base_width = NULL
-# )
-# 
-# save_plot(
-#   file.path(path_to_results, 'figures', 'fig_rho_dup_fixed_n.png'),
-#   fig_rho_dup_v_unq_fixed_n, 
-#   ncol = 1, nrow = 1, base_height = 3.71,
-#   base_asp = 1.618, base_width = NULL
-# )
 
